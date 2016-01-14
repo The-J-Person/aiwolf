c6c79dd8128c4bb579822624e4734e2a02d9e2cf
Gregorius Maximus <rogovin_grisha@hotmail.com>
2016-01-14 21:00:26 +0200

    replaced empty files

--- BFSWolf.cpp
+++ BFSWolf.cpp
@@ -1,1 +1,55 @@
 
+#include "BFSWolf.h"
+#include <queue>
+#include <list>
+
+BFSwolf::BFSwolf()
+{
+    //ctor
+    int width = parent_map.width;
+    int height = parent_map.height;
+    coordinate start = this.get_location();
+    std::queue<coordinate> que;
+    bool visited[width][height] = {false}
+}
+
+BFSwolf::~BFSwolf()
+{
+    //dtor
+}
+
+int BFSwolf::act()
+{
+    que.push(start);
+    set_visit(start);
+    while(!que.empty())
+    {
+        coordinate cor = que.pop();
+        std::list<coordinate> cors = get_cords(cor);
+        for (i=0;i<cors.size();i++)
+        {
+            if(parent_map[cors[i].x][cors[i].y] == RABBIT)
+                move_self()///needd to determine what direction
+                return o;
+            set_visit(cors[i]);
+        }
+    }
+}
+void BFSwolf::set_visit(coordinate cor)
+{
+    visited[cor.x][cor.y] = 1;
+}
+
+list<coordinate> BFSwolf::get_cords(coordinate cor)
+{
+    std::list<coordinate> cors;
+    if(cor.x>0 and visited[x-1][y] == true)
+        cors.push_back(new coordinate(x-1,y));
+    if(cor.x<height and visited[x+1][y] == true)
+        cors.push_back(new coordinate(x+1,y));
+    if(cor.y>0 and visited[x][y-1] == true)
+        cors.push_back(new coordinate(x,y-1));
+    if(cor.y<height and visited[x][y+1] == true)
+        cors.push_back(new coordinate(x,y+1));
+    return cors;
+}
--- BFSWolf.h
+++ BFSWolf.h
@@ -1,1 +1,18 @@
+#ifndef BFSWOLF_H
+#define BFSWOLF_H
+
+#include "wolf.h"
+
+
+class BFSwolf : public wolf
+{
+    public:
+        BFSwolf();
+        virtual ~BFSwolf();
+        int act();
+    protected:
+    private:
+};
+
+#endif // BFSWOLF_H
 
