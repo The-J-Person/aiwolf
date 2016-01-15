bdaad7d1ae9faac614287fba25a853260cee89a1
Gregorius Maximus <rogovin_grisha@hotmail.com>
2016-01-15 21:28:32 +0200

    Added act for lion and rabbit

--- lion.cpp
+++ lion.cpp
@@ -7,7 +7,29 @@ lion::lion()
 
 direction lion::act()
 {
-    return NOWHERE;
+    coordinate cor = get_location();
+	coordinate wcor = parent_map->get_nearest_wolf(cor);
+
+	if (wcor.x == cor.x && wcor.y == cor.y)
+	{
+		return NOWHERE;
+	}
+	else if (wcor.x < cor.x)
+	{
+		return LEFT;
+	}
+	else if (wcor.x < cor.x)
+	{
+		return RIGHT;
+	}
+	else if (wcor.x == cor.x && cor.y> wcor.y)
+	{
+		return UP;
+	}
+	else
+	{
+		return DOWN;
+	}
 }
 lion::~lion()
 {
--- rabbit.cpp
+++ rabbit.cpp
@@ -1,4 +1,6 @@
 #include "rabbit.h"
+#include "map.h"
+#include <list>
 
 rabbit::rabbit()
 {
@@ -7,7 +9,29 @@ rabbit::rabbit()
 
 direction rabbit::act()
 {
-    return NOWHERE;
+    coordinate cor = get_location();
+	coordinate wcor = parent_map->get_nearest_wolf(cor);
+
+	if (wcor.x == cor.x && wcor.y == cor.y)
+	{
+		return NOWHERE;
+	}
+	else if (wcor.x < cor.x)
+	{
+		return RIGHT;
+	}
+	else if (wcor.x < cor.x)
+	{
+		return LEFT;
+	}
+	else if (wcor.x == cor.x && cor.y> wcor.y)
+	{
+		return DOWN;
+	}
+	else
+	{
+		return UP;
+	}
 }
 rabbit::~rabbit()
 {
