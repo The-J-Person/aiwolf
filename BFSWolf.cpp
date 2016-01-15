f27225a8ba8ab6f1ef7b4114725879401aa5aa00
Gregorius Maximus <rogovin_grisha@hotmail.com>
2016-01-15 17:08:16 +0200

    made bfswolf compile

--- BFSWolf.cpp
+++ BFSWolf.cpp
@@ -1,16 +1,12 @@
 
 #include "BFSWolf.h"
+#include "map.h"
 #include <queue>
-#include <list>
 
 BFSwolf::BFSwolf()
 {
     //ctor
-    int width = parent_map.width;
-    int height = parent_map.height;
-    coordinate start = this.get_location();
-    std::queue<coordinate> que;
-    bool visited[width][height] = {false}
+	dead = false;
 }
 
 BFSwolf::~BFSwolf()
@@ -18,38 +14,47 @@ BFSwolf::~BFSwolf()
     //dtor
 }
 
-int BFSwolf::act()
+direction BFSwolf::act()
 {
+	map_object** mp = parent_map->get_grid();
+	coordinate start = get_location();
+	const int width = start.x * 2 - 1;
+	const int height = start.y * 2 - 1;
+	std::queue<coordinate> que;
+
+	bool** visited = new bool*[width];
+	for (int i = 0; i < width; i++)
+	{
+		visited[i] = new bool[height];
+		for (int j = 0; j < height; j++)
+			visited[i][j] = false;
+	}
     que.push(start);
-    set_visit(start);
+	visited[start.x][start.y] = true;
     while(!que.empty())
     {
-        coordinate cor = que.pop();
-        std::list<coordinate> cors = get_cords(cor);
-        for (i=0;i<cors.size();i++)
+        coordinate cor = que.front();
+        std::list<coordinate> cors = get_cords(visited,cor);
+		for (list<coordinate>::const_iterator ci = cors.begin(); ci != cors.end(); ++ci)
         {
-            if(parent_map[cors[i].x][cors[i].y] == RABBIT)
-                move_self()///needd to determine what direction
-                return o;
-            set_visit(cors[i]);
+            if(mp[ci->x][ci->y] == RABBIT)
+                return UP;
+			visited[ci->x][ci->y] = true;
         }
     }
 }
-void BFSwolf::set_visit(coordinate cor)
-{
-    visited[cor.x][cor.y] = 1;
-}
 
-list<coordinate> BFSwolf::get_cords(coordinate cor)
+std::list<coordinate> BFSwolf::get_cords(bool** visited, coordinate cor)
 {
-    std::list<coordinate> cors;
-    if(cor.x>0 and visited[x-1][y] == true)
-        cors.push_back(new coordinate(x-1,y));
-    if(cor.x<height and visited[x+1][y] == true)
-        cors.push_back(new coordinate(x+1,y));
-    if(cor.y>0 and visited[x][y-1] == true)
-        cors.push_back(new coordinate(x,y-1));
-    if(cor.y<height and visited[x][y+1] == true)
-        cors.push_back(new coordinate(x,y+1));
+
+	std::list<coordinate> cors;
+	if (cor.x > 0 && visited[cor.x - 1][cor.y] == true)
+		cors.push_back(coordinate{ cor.x - 1, cor.y });
+	if (cor.x< get_location().x*2-1 && visited[cor.x + 1][cor.y] == true)
+		cors.push_back(coordinate{ cor.x + 1, cor.y });
+	if (cor.y>0 && visited[cor.x][cor.y - 1] == true)
+		cors.push_back(coordinate{ cor.x, cor.y - 1 });
+	if (cor.y < get_location().x * 2 - 1 && visited[cor.x][cor.y + 1] == true)
+		cors.push_back(coordinate{ cor.x, cor.y + 1 });
     return cors;
 }
