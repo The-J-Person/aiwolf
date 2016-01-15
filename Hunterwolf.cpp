3eef71cff58b2e4ddd9df1b3cdfebbd9af7527d4
Gregorius Maximus <rogovin_grisha@hotmail.com>
2016-01-15 21:42:16 +0200

    altered act using map's finding methos

--- Hunterwolf.cpp
+++ Hunterwolf.cpp
@@ -11,37 +11,22 @@ HunterWolf::HunterWolf()
 
 direction HunterWolf::act()
 {
-	coordinate cor, mincor;
-	list<float> dists;
-	list<coordinate> cors = parent_map->get_all_rabbits();
-	int x, y;
-	float res, min = INT_MAX;
-	cor = mincor = get_location();
-	for (list<coordinate>::const_iterator ci = cors.begin(); ci != cors.end(); ++ci)
-	{
-		x = cor.x - ci->x;
-		y = cor.y - ci->y;
-		res = ((y*y) / (x*x));
-		dists.push_back(sqrt(res));
-		if (res < min)
-		{
-			min = res;
-			mincor = {ci->x, ci->y};
-		}
-	}
-	if (mincor.x == cor.x && mincor.y == cor.y)
+	coordinate cor = get_location();
+	coordinate rcor = parent_map->get_nearest_rabbit(cor);
+
+	if (rcor.x == cor.x && rcor.y == cor.y)
 	{
 		return NOWHERE;
 	}
-	else if (mincor.x < cor.x)
+	else if (rcor.x < cor.x)
 	{
 		return LEFT;
 	}
-	else if (mincor.x < cor.x)
+	else if (rcor.x < cor.x)
 	{
 		return RIGHT;
 	}
-	else if (mincor.x == cor.x && cor.y> mincor.y)
+	else if (rcor.x == cor.x && cor.y> rcor.y)
 	{
 		return UP;
 	}
